9e62c9ff88c562ccb678d661804449fabaa27f14
Raul Scarmocin <raulsfreitas1997@hotmail.com>
2023-09-13 23:20:39 -0300

    feat: implementing first functional unit test - Simple Select Query Builder

--- tests/query/test-query-builder.cpp
+++ tests/query/test-query-builder.cpp
@@ -2,10 +2,18 @@
 #define BOOST_TEST_MODULE query_builder
 
 #include <string>
+#include <vector>
 
 #include <boost/test/unit_test.hpp>
 
-BOOST_AUTO_TEST_CASE ( SimpleSelectQueryTest ) {
-	const std::string expected_query = "SELECT test_field FROM test_table;";
-	BOOST_CHECK_EQUAL ( expected_query, "" );
+#include <query/SqlSelect.h>
+
+BOOST_AUTO_TEST_CASE(SimpleSelectQueryTest){
+    const std::string expected_query = "SELECT * FROM test_table;";
+
+    const std::string actual_query =
+      Select({ { "*" } })
+      .From("test_table");
+
+    BOOST_CHECK_EQUAL(expected_query, actual_query);
 }
