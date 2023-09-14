d996716b19f6e3f5691306710290070014d5e582
Raul Scarmocin <raulsfreitas1997@hotmail.com>
2023-09-13 23:18:59 -0300

    feat: implement constructor with fields and From (builder design pattern) on Select

--- src/query/SqlSelect.cpp
+++ src/query/SqlSelect.cpp
@@ -1,8 +1,32 @@
 #include "SqlSelect.h"
 
 Select::operator std::string() const {
-    std::string query = "SELECT ";
+    std::string query = "SELECT";
+
+    for (int i = 0; i < _fields.size(); i++) {
+        query += _fields[i];
+        if (i < _fields.size() - 1) {
+            query += ", ";
+        }
+    }
+
+    // TODO make expection case no table
+    query += _table;
+    // or maybe not, user problems...
 
     query += ";";
     return query;
+}
+
+Select::Select(const std::vector<AliasField>& fields) : _fields{fields}{
+}
+
+Select& Select::From(const std::string& table){
+    _table = Table(table);
+    return *this;
+}
+
+Select& Select::From(const Table& table){
+    _table = table;
+    return *this;
 }
--- src/query/SqlSelect.h
+++ src/query/SqlSelect.h
@@ -1,11 +1,24 @@
 #ifndef CPPUTILS_QUERY_SQLSELECT_H_
 #define CPPUTILS_QUERY_SQLSELECT_H_
 
+#include <vector>
+#include <string>
+
 #include "SqlClause.h"
+#include "SqlAliasField.h"
+#include "SqlTable.h"
 
 class Select : public Clause {
 public:
+    explicit Select(const std::vector<AliasField>& fields = {});
     virtual operator std::string() const;
+
+    Select& From(const std::string& table);
+    Select& From(const Table& table);
+
+private:
+    std::vector<AliasField> _fields;
+    Table _table;
 };
 
 #endif // CPPUTILS_QUERY_SQLSELECT_H_
