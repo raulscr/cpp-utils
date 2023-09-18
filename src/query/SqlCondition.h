308159d1dedcff2e92986f00e0b44cafd1096142
raulscr <raulsfreitas1997@hotmail.com>
2023-09-18 20:37:14 -0300

    feat: Implement Condition::operator

--- src/query/SqlCondition.h
+++ src/query/SqlCondition.h
@@ -7,6 +7,19 @@
 
 class Condition : public Clause {
 public:
+    enum Operator {
+        IS,
+        IS_NOT,
+        LIKE,
+        EQUAL,
+        NOT_EQUAL,
+        LESS_THAN,
+        LESS_EQUAL,
+        GREATER_THAN,
+        GREATER_EQUAL,
+        // TODO: investigate possible operators and implement them
+    };
+
     explicit Condition(const Condition& other);
     Condition(const std::string& value = "");
     virtual operator std::string() const;
@@ -15,9 +28,9 @@ public:
 
     Condition& IsEqual(const std::string& other);
     Condition& IsNull();
-    // TODO: investigate possible operators and implement them
 
 protected:
+    Operator _operator;
     // TODO: use SqlValue (not implemented yet)
     std::string _first_value;
     std::string _second_value;
